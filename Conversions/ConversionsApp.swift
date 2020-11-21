15a4bb87995537d38f6044c050905aa446b4d9e0
Pranav Kasetti <pranavkasetti@gmail.com>
2020-11-21 16:42:21 +0000

    Initial Commit

---
+++ Conversions/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Conversions
+//
+//  Created by Pranav Kasetti on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
